6f39c9e19f3027a861acb5543a029b06da3a95f9
Aamir Masood <aamir@soladev.net>
2018-03-28 17:26:56 +0530

    Navbar completed

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ $(".menu-icon").click(function(){
 	//Adds class to .menu icon so CSS can change icon shape
 	$(this).toggleClass( "active" );
 	//Adds mobile nav so CSS can change make it appear
-	$(".nav-area").toggleClass( "open" );
+	$(".nav-area-mobile").toggleClass( "open" );
 	//Stops scrolling when nav is open
 	$("html").toggleClass( "overflow-hidden" );
 });
@@ -17,7 +17,7 @@ $(".menu-icon").click(function(){
 
 //Target all .nav-area li items that have UL as children add class
 //Mobile & Tablet
-var subMenuItems = $( ".nav-area > ul > li:has(ul)" );
+var subMenuItems = $( ".nav-area-mobile > ul > li:has(ul)" );
 // var menuChild = $('<button class="more"></button>');
 
 $(subMenuItems).addClass("child-menu");
